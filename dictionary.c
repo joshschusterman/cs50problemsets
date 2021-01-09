de108c2700fcdb5d0ca75150bf648821c111605f
joshschusterman <josh.schusterman@gmail.com>
2021-01-09 09:31:13 -0600

    dictionary.c - pset5 - COMPLETE
    Admittedly I don't understand some of what's going on here, the hash function in particular. But it did help strengthen my understanding of linked lists (a little).

--- dictionary.c
+++ dictionary.c
@@ -1,7 +1,12 @@
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <ctype.h> // this is for the tolower function we used.
+#include <string.h> // for strlen?
+#include <strings.h> // for strcasecmp
+#include <stdio.h> // for FILE function.
+#include <cs50.h> // just in case?
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -12,8 +17,14 @@ typedef struct node
 }
 node;
 
-// Number of buckets in hash table - *** Setting to 26 for now, one for each alphabet letter.
+// Number of buckets in hash table
+// So this means the number of "buckets" we're going to have is...
+// 'z' times 45, because z has the largets ASCII value, and words can be up
+// up to 45 characters long. The + 1 I guess is for extra room?
 const unsigned int N = (LENGTH + 1) * 'z';
+
+// Initialize the word count so we can increment as we add words to the table.
+int word_count = 0;
 
 // Hash table
 node *table[N];
@@ -21,7 +32,21 @@ node *table[N];
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    // Get the hash index (which "bucket" the word is in) simply by calling the hash function.
+    int index = hash(word);
+    // Set up the "cursor" that will traverse through the elements in this bucket (table[index])
+    node *cursor = table[index];
+    // Loop through until we hit a node that is NULL (signifying end of the bucket).
+    while (cursor != NULL)
+    {
+        // See if the current word in the bucket matches the input word.
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        // Go to the next node (word) in this bucket.
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -50,18 +75,22 @@ bool load(const char *dictionary)
     }
     // Initialize the "word" array?
     char word[LENGTH + 1];
+    // arguments for fscanf = 1. file you're reading from, 2. format of the element (string in this case), 3. ???
     while (fscanf(file, "%s", word) != EOF) // EOF is returned when we've reached the end of the file.
     {
         // create a node at address n, allocate memory the size of a node.
         node *n = malloc(sizeof(node));
-        // arguments for fscanf = 1. file you're reading from, 2. format of the element (string in this case), 3. ???
-        fscanf(file, "%s", word);
+        if (n == NULL)
+        {
+            return false;
+        }
+
         // arguments for strcpy = 1. the DESTINATION, this case the word "field" (array) IN node n, 2. the source word (element)
         strcpy(n->word, word);
         // set the next pointer, but we don't know what it is yet.
         n->next = NULL;
         // next step is to run the hash function to get the index (in the linked list) where the word will go.
-        wordIndex = hash(word); // * not sure about argument yet. Needs *?
+        int wordIndex = hash(word); // * not sure about argument yet. Needs *?
         // if the index of this part of the table's node is NULL, place this new word (via where we copied it to)...
         // in that index spot.
         if(table[wordIndex] == NULL)
@@ -70,26 +99,41 @@ bool load(const char *dictionary)
         }
         else
         {
-            // *** I UNDERSTAND the visual explanation in the walkthrough, but how this code does it.
+            // *** I UNDERSTAND the visual explanation in the walkthrough, but not how this code does it.
             // Ok, FIRST we make n's pointer (next) point to the first index of this table (???)
             n->next = table[wordIndex];
             // Then make the value of the first index the word we just copied to n (???)
             table[wordIndex] = n;
         }
+        word_count++;
+
     }
-    return false;
+    fclose(file);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+    // Need to iterate over every element in the array "table" (our array of linked lists).
+    for (int i = 0; i < N; i++)
+    {
+        node *head = table[i];
+        node *cursor = head;
+        node *tmp = head;
+
+        while (cursor != NULL)
+        {
+            cursor = cursor->next;
+            free(tmp);
+            tmp = cursor;
+        }
+    }
+    return true;
 }
