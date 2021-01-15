87c4047617bead53799cd3d82492eb1114bf2979
joshschusterman <josh.schusterman@gmail.com>
2021-01-15 17:17:40 -0600

    mario-more.py - pset6

---
+++ mario-more.py
@@ -0,0 +1,12 @@
+height = 0
+
+while True:
+    try:
+        height = int(input("Height: "))
+        if height in range(1, 9):
+            break
+    except:
+        continue
+
+for i in range(1, height + 1):
+    print((" " * (height - i)) + ("#" * i) + "  " + ("#" * i))
